1dc895ee7b27964ff40ea36e09377bb59bc9f168
egarcerant-collab <egarcerant@dusakawiepsi.com>
2026-06-02 16:58:07 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 //  Versión: 5.0
 // ================================================================
 
-const CACHE = 'dusakawi-mapsdis-v6';
+const CACHE = 'dusakawi-mapsdis-v7';
 const ASSETS = [
   './manifest.json',
   './logo_dusakawi.png'
